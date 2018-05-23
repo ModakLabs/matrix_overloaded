b945e292f5269d9da9df59c33097e8526f705ebd
Prasun Anand <prasunanand.bitsp@gmail.com>
2018-05-23 13:21:42 +0530

    setup matrix multiplication

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # matrix_overloaded
 Testing Matrix threshold size on the GPU devices
+
+
+# Build
+```
+make
+```
+
+# Run
+
+```sh
+ ./build/matrix_overloaded --size=10 --display=true
+```
--- source/matrix_overloaded/app.d
+++ source/matrix_overloaded/app.d
@@ -1,8 +1,17 @@
 module matrix_overloaded.app;
 
+import std.getopt;
 import std.stdio;
 
-void main()
-{
-	writeln("hello");	
+import matrix_overloaded.cuda;
+
+void main(string[] args){
+  int option_size;
+  string display_flag = "false";	
+  getopt(args,
+  	"size", &option_size,
+  	"display", &display_flag
+  	);
+  writeln("Matrix Multiplication called");
+  call_gemm_routine(display_flag, option_size);
 }
